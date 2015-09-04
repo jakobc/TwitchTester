9eddf349af7f06a4f779e5c734a32183900343cd
Jakob C <jakobce@gmail.com>
2015-09-04 18:05:44 +0200

    Initial Commit

---
+++ TwitchTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitchTester
+//
+//  Created by Jakob Cederlund on 2015-09-04.
+//  Copyright (c) 2015 dev4phone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
